a288e58c67e08d441b37bbea18326c3d343daba4
nmacambira <nmacambira@gmail.com>
2013-04-17 22:44:58 +0000

    Valmar segue o botoesServlet para gerenciar as opções de criar, alterar e excluir no.

--- TI_Trabalho_Teste/src/servlets/BotoesServlet.java
+++ TI_Trabalho_Teste/src/servlets/BotoesServlet.java
@@ -1,12 +1,14 @@
 package servlets;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import organograma.Info;
+import organograma.No;
 
 import DAO.InfoDAO;
 import DAO.NoDAO;
@@ -17,20 +19,73 @@ import DAO.NoDAO;
 @WebServlet("/BotoesServlet")
 public class BotoesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BotoesServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		String criar = request.getParameter("criar");
-		String alterar = request.getParameter("alterar");
-		String excluir = request.getParameter("excluir");
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		boolean criarPressed = request.getParameter("criar") != null;
+		boolean alterarPressed = request.getParameter("alterar") != null;
+		boolean excluirPressed = request.getParameter("excluir") != null;
 		
-		NoDAO no = new NoDAO();
-		InfoDAO info = new InfoDAO();
+//		String resp = request.getParameter("acao");
+		NoDAO noDao = new NoDAO();
+		InfoDAO infoDao = new InfoDAO();
+		
+		No noServlet = new No(request.getParameter("nomeNo"), "", Integer
+				.parseInt(request.getParameter("no-pai")));
+		
+		Info infoServlet = new Info(Integer.parseInt(request.getParameter("ramal")),
+				Integer.parseInt(request.getParameter("sala")), request
+				.getParameter("representante"), request
+				.getParameter("email"), Integer.parseInt(request
+				.getParameter("no-pai")));
+		
+		if (criarPressed){
+			noDao.saveData(noServlet);			
+			infoDao.saveData(infoServlet);
+			
+			request.getRequestDispatcher("AdminServlet").forward(request, response);
+			
+		}
+		
+		if (alterarPressed){
+			noDao.updateData(noServlet);
+			infoDao.updateData(infoServlet);
+			
+			request.getRequestDispatcher("AdminServlet").forward(request, response);
+			
+		}
+		
+		if (excluirPressed){
+			
+			noDao.deleteData(noServlet);
+			infoDao.deleteData(infoServlet);
+			
+			request.getRequestDispatcher("AdminServlet").forward(request, response);
+			
+			
+		}
+		
+		
+		
+		
+		
+		
 	}
 
-	
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
 
